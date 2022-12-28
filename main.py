cc222593b3e071b1bf02ae2f214838075bbd2f45
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-12-28 13:16:35 -0600

    timer works, break labels, work labels, etc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
-# ---------------------------- CONSTANTS ------------------------------- #
+import math
+#CONSTANTS:
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -8,14 +9,41 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# TIMER MECHANISM 
+def start_timer():
+    global reps
+    reps +=1 
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = SHORT_BREAK_MIN * 60
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-
-# ---------------------------- UI SETUP ------------------------------- #
+    if reps % 8 == 0:
+       count_down(long_break_sec)  
+       title_label.config(text="Break", fg=RED)
+    elif reps % 2 ==0:
+        count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
+   
+#COUNTDOWN MECHANISM: 
+def count_down(count):
+    count_min = math.floor(count/60) #minutes
+    count_sec = count%60 #seconds remaining after minutes
+    if count_sec <10:
+        count_sec= f"0{count_sec}"
+    #change canvas element using itemconfig() for the specific canvas:
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count-1) #use this instead of time module
+    else:
+        start_timer()
+#UI SETUP:
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
@@ -27,10 +55,10 @@ title_label.grid(column=1, row=0)
 canvas = Canvas(width=200 ,height=224, bg=YELLOW, highlightthickness=0)#highlightthickness = 0 get rid of border around canvas
 tomato_img=PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image= tomato_img) #x, y, image as PhotoImage
-canvas.create_text(100, 130, text= "00:00", fill="white", font=(FONT_NAME, 35, "bold")) #same values as the image for position
-canvas.grid(column=1, row=1)
+timer_text = canvas.create_text(100, 130, text= "00:00", fill="white", font=(FONT_NAME, 35, "bold")) #same values as the image for position
+canvas.grid(column=1, row=1) 
 
-start_button = Button(text="Start", highlightthickness=0)
+start_button = Button(text="Start", highlightthickness=0, command = start_timer) #command binds button to a function
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", highlightthickness=0)
